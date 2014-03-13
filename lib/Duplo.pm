00b8d7c72107d5f18543dd7c59da3cec38c86de1
Eric Lorenzana <self@elrzn.me>
2014-03-13 19:24:21 +0000

    Initial commit

---
+++ lib/Duplo.pm
@@ -0,0 +1,33 @@
+package Duplo;
+use strict;
+use warnings;
+
+my @HTML_KEYWORDS;
+
+BEGIN {
+  @HTML_KEYWORDS = qw(
+    body
+    div
+    head
+    html
+    li
+    p
+    ul
+  );
+}
+
+use Exporter::Tidy default => [@HTML_KEYWORDS];
+
+sub _with_name {
+  my $name = shift;
+  my @p;
+  if (@_ && ref $_[0] eq 'HASH') {
+    my $h = shift;
+    @p = map { sprintf " %s=\"%s\"", $_, $h->{$_} } keys %$h;
+  }
+  "<$name@p>" . join('', @_) . "</$name>";
+}
+
+eval "sub $_ { _with_name '$_', \@_ }" for @HTML_KEYWORDS;
+
+1;
