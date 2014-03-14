f0a9a8f2a437538ae154e3297b4e0d679048fe76
elrzn <self@elrzn.me>
2014-03-14 12:38:14 +0100

    Add support for single tags

--- lib/Duplo.pm
+++ lib/Duplo.pm
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 my @HTML_KEYWORDS;
+my @HTML_KEYWORDS_SINGLE;
 
 BEGIN {
   @HTML_KEYWORDS = qw(
@@ -15,20 +16,26 @@ BEGIN {
     title
     ul li
   );
+  @HTML_KEYWORDS_SINGLE = qw(
+    img
+  );
 }
 
-use Exporter::Tidy default => [@HTML_KEYWORDS];
+use Exporter::Tidy default => [@HTML_KEYWORDS,
+                               @HTML_KEYWORDS_SINGLE];
 
 sub _with_name {
+  my $treatment = shift;
   my $name = shift;
   my @p;
   if (@_ && ref $_[0] eq 'HASH') {
     my $h = shift;
     @p = map { sprintf " %s=\"%s\"", $_, $h->{$_} } keys %$h;
   }
-  "<$name@p>" . join('', @_) . "</$name>";
+  "<$name@p>" . join('', @_) . ($treatment eq ':normal' ? "</$name>" : q{});
 }
 
-eval "sub $_ { _with_name '$_', \@_ }" for @HTML_KEYWORDS;
+eval "sub $_ { _with_name ':normal', '$_', \@_ }" for @HTML_KEYWORDS;
+eval "sub $_ { _with_name ':single', '$_', \@_ }" for @HTML_KEYWORDS_SINGLE;
 
 1;
