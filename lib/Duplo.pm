06d45fce8b23204e54f008e73df2f356461f93d5
Eric Lorenzana <self@elrzn.me>
2014-03-24 10:31:44 +0000

    Move _with_name within do scope

--- lib/Duplo.pm
+++ lib/Duplo.pm
@@ -36,23 +36,23 @@ BEGIN {
 
 use Exporter::Tidy default => [map { _ $_ } @HTML_TAGS, @HTML_TAGS_SINGLE];
 
-sub _with_name {
-  my $treatment = shift;
-  my $name = substr shift, 0, -1;
-  my @p;
-  if (@_ && ref $_[0] eq 'HASH') {
-    my $h = shift;
-    @p = map { sprintf " %s=\"%s\"", $_, $h->{$_} } keys %$h;
+do {    # Build subroutines.
+  sub _with_name {
+    my $treatment = shift;
+    my $name = substr shift, 0, -1;
+    my @p;
+    if (@_ && ref $_[0] eq 'HASH') {
+      my $h = shift;
+      @p = map { sprintf " %s=\"%s\"", $_, $h->{$_} } keys %$h;
+    }
+    "<$name@p>" . join('', @_) . ($treatment eq ':normal' ? "</$name>" : q{});
   }
-  "<$name@p>" . join('', @_) . ($treatment eq ':normal' ? "</$name>" : q{});
-}
 
-do {
-  ## Build subroutines.
   sub _builder {
     my ($sub_name, $type) = @_;
     qq(sub $sub_name { _with_name '$type', '$sub_name', \@_ });
   }
+
   eval _builder $_, q(:normal) for map { _ $_ } @HTML_TAGS;
   eval _builder $_, q(:single) for map { _ $_ } @HTML_TAGS_SINGLE;
 };
